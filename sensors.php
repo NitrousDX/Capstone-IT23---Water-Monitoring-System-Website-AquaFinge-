f50656d79162f6e6af570414ec807b4a38a25a18
NitrousDX <neil.nitrousdx@gmail.com>
2024-09-15 19:18:00 +0800

    Addded AquaFinge Icon

--- sensors.php
+++ sensors.php
@@ -14,7 +14,8 @@ if (!isset($_SESSION['logged_in_user'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>AquaFinge</title>
+    <link rel="icon" type="image/x-icon" href="images/AquaFinge.png">
     <link rel="stylesheet" href="styles/test.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.4/raphael-min.js"></script>
